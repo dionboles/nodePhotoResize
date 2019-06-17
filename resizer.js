ac7f2063fea76b9743dec869e8ca86ff9bbbe567
Dion Boles <dion.boles@gmail.com>
2019-06-17 10:39:45 -0400

    added more error checking

--- resizer.js
+++ resizer.js
@@ -2,15 +2,36 @@ const Jimp = require('jimp');
 const fs = require("fs");
 const path = require("path");
 
- fs.readdir("./src/img/",function(err,data){
-    data.forEach(element => {
-        if (path.extname(element) == '.jpg'){
-            console.log(path.resolve(element));
-            Jimp.read( __dirname+"/src/img/"+element).then(tmp => tmp.resize(1920,795).quality(60).write("build/img/"+path.basename(element))).catch(err =>{
-                console.error(err);
-            })
+
+// Make the build folder this is your output folder
+const buildDir = './build/img';
+
+if (!fs.existsSync(buildDir)){
+    fs.mkdirSync(buildDir,{ recursive: true });
+}
+
+// Make the src folder this is your input folder
+const srcDir = './src/img';
+
+if (!fs.existsSync(srcDir)){
+    fs.mkdirSync(srcDir,{ recursive: true });
+}
+
+// this loops over files in the src dir
+if (fs.existsSync(srcDir)){
+    console.log("Please wait working on files");
+    fs.readdir("./src/img/",function(err,data){
+        data.forEach(element => {
+            if (path.extname(element) == '.jpg'){
+                Jimp.read( __dirname+"/src/img/"+element).then(tmp => tmp.resize(1920,795).quality(60).writeAsync("build/img/"+path.basename(element))).catch(err =>{
+                    console.error(err);
+                })
+            }
+        });
+        if (err){
+            console.error(err);
         }
-        
-    });
-    
-})
+    })
+}else{
+    console.log("Src Folder not created");
+}
